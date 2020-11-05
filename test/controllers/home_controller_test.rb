534bd96e0ffd00991d8fe33d7bf6e8be0cf286ae
Ryan Slater <32707042+rslater-cs@users.noreply.github.com>
2020-11-05 22:08:21 +0000

    Added log in field

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
   end
 
