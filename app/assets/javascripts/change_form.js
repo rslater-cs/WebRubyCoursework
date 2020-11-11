2d3a82d8d70c0ea5b11448f4cf53d7810915358c
Ryan Slater <32707042+rslater-cs@users.noreply.github.com>
2020-11-11 23:09:36 +0000

    created form animation

---
+++ app/assets/javascripts/change_form.js
@@ -0,0 +1,37 @@
+function clicked() {
+    const button = $("#input_button");
+    const height = button.outerHeight();
+    const register = $("#register_fields");
+    if(button.text() === "Register"){
+        anim_log_to_reg(button, height, register);
+    }else{
+        anim_reg_to_log(button, height, register);
+    }
+}
+
+function anim_log_to_reg(button, height, register){
+    button.animate({opacity: "0", height: "0px", top: "+="+height.toString()+"px"}, 300);
+    button.queue(function(){
+        button.html("Log In");
+        button.dequeue();
+    });
+    button.animate({opacity: "1", height: height}, 300);
+    register.animate({height: "+=50px"}, 300);
+    register.html("<label>Re-Enter Password</label><input type='password' name='reenter'>");
+    register.animate({opacity: "1"}, 300);
+}
+
+function anim_reg_to_log(button, height, register){
+    button.animate({opacity: "0", height: "0px"}, 300);
+    register.animate({opacity: "0"}, 300);
+    button.queue(function(){
+        button.html("Register");
+        button.dequeue();
+    });
+    button.animate({opacity: "1", height: height, top: "-="+height.toString()+"px"}, 300);
+    register.animate({height: "-=50px"}, 300);
+    register.queue(function(){
+       register.html("");
+       register.dequeue();
+    });
+}
